690a2bf983ff3ad47c928e4e99390ef8b2e17c86
Yana <yanaegorova69.3@gmail.com>
2022-09-28 01:39:33 +0300

    +

--- js/data/products.js
+++ js/data/products.js
@@ -378,4 +378,6 @@ export const products = [
         isItRing: false,
         ringSizes: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'],
     },
+
+
 ]
--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -1,26 +1,27 @@
+
+
 export const item = (product) => {
-
   return `
-  <li class="item"> 
-   <div class="product__block js_product glass glass-m" id="${product.id}">
-      <div class="product__image-block">
-          <img src="${product.image}" alt="" class="img">
-      </div>
-      <div class="text-block">
-      <div class="text-box">
-      <p class="product__name js_product-name">${product.name}</p>
-      <span class="product__price">
-      $${(product.price).toFixed(2)}
-      </span>
-      </div>
-      <div class="product__btn-box">
-      <a href="product-page.html?id=${product.id}" class="product__btn">product details</a>
-      <button class="product__btn js_add-to-cart">add to cart</button>
-      </div>
-      </div>
+    <li class="item">
+    <div class="product__block js_product glass glass-m" id="${product.id}">
+    <div class="product__image-block">
+     <img src="${product.image}" alt="" class="img">
     </div>
-  </li>
+    <div class="text-block">
+    <div class="product__text-box">
+    <p class="product__name js_product-name">${product.name}</p>
+      <span class="product__price">$${(product.price).toFixed(2)}</span>
+    </div>
+    <div class="product__btn-block">
+    <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+    ${
+      product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+    }
+    </div>
+    </div>
+
+
+    </div>
+    </li>
     `
-}
-
-
+  }
